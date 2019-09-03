bf49d89fd262a63f8ab942fe2c336d092c1ec119
tan0928 <1641512260@qq.com>
2019-09-03 13:53:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/study/dbHelper.java
@@ -0,0 +1,32 @@
+package com.example.user.study;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class dbHelper extends SQLiteOpenHelper {
+    public static final String TB_NAME = "friends";
+
+    public dbHelper(Context context, String name, CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        db.execSQL("CREATE TABLE IF NOT EXISTS " +
+                TB_NAME + " ( _id integer primary key autoincrement," +//
+                "name varchar," +
+                "age integer" +
+                ") ");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO Auto-generated method stub
+        db.execSQL("DROP TABLE IF EXISTS " + TB_NAME);
+        onCreate(db);
+    }
+}
